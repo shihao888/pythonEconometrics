a51272a63c4c2a823c02190421203c175b11d7f0
shihao888 <sheraton@sina.com>
2022-12-30 23:20:47 +0800

    3rd commit

---
+++ 5.一元线性回归（读入EXCEL）.py
@@ -0,0 +1,10 @@
+# Author: 史浩 浙江金融职业学院
+# --------------------------------------------------------
+import pandas as pd
+import statsmodels.api as sm
+df = pd.read_excel(r'data\myexcel.xlsx')
+X = sm.add_constant(df.iloc[:, 1])
+print(X)
+model = sm.OLS(df.iloc[:, 0], X)
+result = model.fit().summary()
+print(result)
